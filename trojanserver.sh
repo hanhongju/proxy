208d21730bcef00d71aa603aa7e7d1ca653b5392
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-09-03 08:50:44 +0800

    Update trojanserver.sh

--- trojanserver.sh
+++ trojanserver.sh
@@ -68,10 +68,10 @@ echo '
 systemctl    enable      trojan
 systemctl    restart     trojan
 #显示监听端口
-sleep 1s
-/usr/bin/trojan  -t  
+sleep      1s
+trojan    -t
+netstat   -plnt
 OUTPUT=$(netstat -plnt | grep 'trojan'    2>&1)
-echo   $OUTPUT
 if     [[  "$OUTPUT"   =~   "trojan"   ]]
 then        echo   "至此，trojan可正常工作。服务器密码为fengkuang。"
 else        echo   "您输入的域名地址可能没有正确解析或者短时间申请了太多的证书，不能正常申请证书，所以trojan不能正常工作。在您确认了域名解析没有问题后再请重新运行本脚本。"
