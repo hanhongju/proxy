7419c3e4f2145daf6566f1c54cf106663688a484
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-10-10 17:23:49 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,16 +1,3 @@
-#Docker官方仓库安装@Debian 10
-apt   update
-apt   full-upgrade  -y    --fix-missing
-apt   autoremove    -y
-apt   install       -y      apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-curl         -fsSL          https://download.docker.com/linux/debian/gpg    |  apt-key add -
-add-apt-repository         "deb [arch=amd64] https://download.docker.com/linux/debian    $(lsb_release -cs)    stable"
-apt   update
-apt   install       -y      docker-ce docker-ce-cli containerd.io
-docker      run      hello-world
-
-
-
 #Docker官方安装脚本安装
 apt   update
 apt   install   -y    curl
@@ -26,20 +13,4 @@ docker    run        -d        --name      v2ray      -p       8000:8000     -v 
 docker    logs        v2ray
 docker    container   ls
 #回显容器信息
-
-
-
-#Docker安装v2ray+tls server@Debian 10
-#定义站点地址
-site=<domain>
-#拉取v2ray脚本并安装
-apt    purge    -y     apache2
-docker    rm    -f     v2ray
-docker    run   -d    --name    v2ray    -p   443:443   -p   80:80   -v   $HOME/.caddy:/root/.caddy    pengchujin/v2ray_ws:0.10   $site   V2RAY_WS  15448fce-7c71-11ea-bc55-0242ac130003  
-sleep     3s
-docker    logs         v2ray
-docker    container    ls
-#显示服务器配置
-
-
 
