af8215e09516cd0c5d33c7702175f497e85a2db2
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-12-25 23:11:29 +0800

    Update vlessserver.sh

--- vlessserver.sh
+++ vlessserver.sh
@@ -57,7 +57,7 @@ echo '
             "protocol": "vless",
             "settings":{
                        "clients":   [{"id": "8c38d360-bb8f-11ea-9ffd-c182155e578a"}],
-                       "fallbacks": [{"dest": "ualcan.path.uab.edu:80"}],
+                       "fallbacks": [{"dest": "www.rodong.rep.kp:80"}],
                        "decryption": "none"
                        },
             "streamSettings": {
