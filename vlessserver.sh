78219e50b430feb71d72da0c1f97ae472207d497
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-12-25 21:01:04 +0800

    Update vlessserver.sh

--- vlessserver.sh
+++ vlessserver.sh
@@ -1,3 +1,23 @@
+# V2rayserver安装脚本 @ Debian 10 or Ubuntu 20.04
+echo    "
+本脚本可以自动安装v2ray，自动申请并使用tls证书加密保护v2ray的流量，反代美国国家生物技术信息中心网址进行网站伪装。需要您事先将此VPS的IP地址解析到一个有效域名上。
+如果此VPS使用KVM虚拟技术，此脚本自动开启BBR加速。安装完成后v2ray配置:
+端口为             443
+用户ID为           8c38d360-bb8f-11ea-9ffd-c182155e578a
+额外ID为           0
+传输协议为         ws
+路径为            /world
+底层传输安全为     tls
+理解并记录下这些信息后请按回车键继续，并在下一栏输入您解析的有效域名。如果域名输入有误请按Ctrl+C终止脚本运行，然后重新运行脚本。
+"
+read    nothing
+echo    "请输入此VPS的IP对应的域名地址："
+read    site
+echo    "好的，现在要开始安装了。"
+sleep   5s
+
+
+
 
 #计时
 begin=$(date +%s)
@@ -29,7 +49,7 @@ echo '
                                   "level": 0
                                   }],
                        "decryption": "none",
-                       "fallbacks": [{"www.baidu.com:443"}]
+                       "fallbacks": [{"dest":"www.baidu.com:443"}]
                         },
             "streamSettings": {
                               "network": "tcp",
@@ -46,6 +66,12 @@ echo '
 "outbounds": [{"protocol": "freedom"}]
 }
 '     >     /usr/local/etc/v2ray/config.json
+
+
+
+
+
+
 #申请SSL证书
 service   nginx   stop
 certbot   certonly    --standalone    --agree-tos     -n     -d      $site     -m    86606682@qq.com 
@@ -61,16 +87,17 @@ echo       "
 "      |      crontab
 service       cron      restart
 #启动V2Ray和Nginx：
-systemctl   enable     v2ray
-systemctl   restart    v2ray
+systemctl     enable     v2ray
+systemctl     restart    v2ray
 #验证配置文件，显示监听端口
+sleep       1s
 v2ray      -test        -config=/usr/local/etc/v2ray/config.json
 sysctl     -p
 crontab    -l
 netstat    -plnt
 #如果nginx配置有错误，重置nginx配置文件
-OUTPUT=$(nginx -t 2>&1)
-if     [[  "$OUTPUT"   =~   "successful"   ]]
+OUTPUT=$(netstat -plnt | grep 'v2ray'    2>&1)
+if     [[  "$OUTPUT"   =~   "v2ray"   ]]
 then        echo   "至此，v2ray可正常工作。"
 else        echo   "您输入的域名地址可能没有正确解析或者短时间申请了太多的证书，不能正常申请证书，所以nginx不能正常工作。现在所有nginx配置都已被删除。在您确认了域名解析没有问题后再请重新运行本脚本。"
             rm    -rf    /etc/nginx/sites-enabled/*
