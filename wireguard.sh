68cfc3a447fdf66326b8fd7fd7c62edaed9ddf1c
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-12-26 00:15:27 +0800

    Update wireguard.sh

--- wireguard.sh
+++ wireguard.sh
@@ -3,7 +3,10 @@ apt   update
 apt   install   -y   wireguard net-tools
 
 #开启ipv4流量转发
-echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+echo "
+net.ipv4.ip_forward = 1
+net.ipv6.conf.all.forwarding = 1
+" >> /etc/sysctl.conf
 sysctl   -p
 systemctl enable wg-quick@wg0
 
