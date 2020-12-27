5fa201dfb80e36f7d1cee39a1c91ea196492244c
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-12-27 11:18:23 +0800

    Update wireguard.sh

--- wireguard.sh
+++ wireguard.sh
@@ -23,20 +23,30 @@ echo $ipv4
 echo $ipv6
 
 
+
 #生成服务器配置文件
 echo  "
 [Interface]
 PrivateKey = $(cat pri1)
-Address = 192.0.2.1/24,2001:DB8::1/64
+Address    = 10.0.0.1/24
 ListenPort = 500
-PostUp   = iptables  -A FORWARD -i wg0 -j ACCEPT; iptables  -A FORWARD -o wg0 -j ACCEPT; iptables  -t nat -A POSTROUTING -o $interface4 -j MASQUERADE
-PostUp   = ip6tables -A FORWARD -i wg0 -j ACCEPT; ip6tables -A FORWARD -o wg0 -j ACCEPT; ip6tables -t nat -A POSTROUTING -o $interface6 -j MASQUERADE
-PostDown = iptables  -D FORWARD -i wg0 -j ACCEPT; iptables  -D FORWARD -o wg0 -j ACCEPT; iptables  -t nat -D POSTROUTING -o $interface4 -j MASQUERADE
-PostDown = ip6tables -D FORWARD -i wg0 -j ACCEPT; ip6tables -D FORWARD -o wg0 -j ACCEPT; ip6tables -t nat -D POSTROUTING -o $interface6 -j MASQUERADE
+PostUp     = iptables  -A FORWARD -i wg0 -j ACCEPT; iptables  -A FORWARD -o wg0 -j ACCEPT; iptables  -t nat -A POSTROUTING -o $interface4 -j MASQUERADE
+PostDown   = iptables  -D FORWARD -i wg0 -j ACCEPT; iptables  -D FORWARD -o wg0 -j ACCEPT; iptables  -t nat -D POSTROUTING -o $interface4 -j MASQUERADE
 [Peer]
 PublicKey  =  $(cat pub2)
-AllowedIPs =  0.0.0.0/0, ::/0
-"    >     /etc/wireguard/wg0.conf
+AllowedIPs =  10.0.0.2/32
+"    >     /etc/wireguard/wg4.conf
+echo  "
+[Interface]
+PrivateKey = $(cat pri1)
+Address    = 2001:DB8::1/64
+ListenPort = 500
+PostUp     = ip6tables -A FORWARD -i wg0 -j ACCEPT; ip6tables -A FORWARD -o wg0 -j ACCEPT; ip6tables -t nat -A POSTROUTING -o $interface6 -j MASQUERADE
+PostDown   = ip6tables -D FORWARD -i wg0 -j ACCEPT; ip6tables -D FORWARD -o wg0 -j ACCEPT; ip6tables -t nat -D POSTROUTING -o $interface6 -j MASQUERADE
+[Peer]
+PublicKey  =  $(cat pub2)
+AllowedIPs =  2001:DB8::2/128
+"    >     /etc/wireguard/wg6.conf
 
 
 
@@ -44,28 +54,38 @@ AllowedIPs =  0.0.0.0/0, ::/0
 echo  "
 [Interface]
 PrivateKey =  $(cat pri2)
-Address    =  192.0.2.1/24,2001:DB8::/64
-DNS        =  8.8.8.8, 2001:4860:4860::8888
+Address    =  10.0.0.2/24
+DNS        =  8.8.8.8
 [Peer]
 PublicKey  =  $(cat pub1)
-AllowedIPs =  0.0.0.0/0, ::/0
+AllowedIPs =  0.0.0.0/0
+Endpoint   =  $ipv4:500
+"    >        client4.conf
+echo  "
+[Interface]
+PrivateKey =  $(cat pri2)
+Address    =  2001:DB8::2/64
+DNS        =  2001:4860:4860::8888
+[Peer]
+PublicKey  =  $(cat pub1)
+AllowedIPs =  ::/0
 Endpoint   =  [$ipv6]:500
-"    >        client.conf
+"    >        client6.conf
 
-
-echo    "--------以下是客户端配置文件，请保存并在客户端中使用---------"
-cat      client.conf
 
 
 #启动服务
-wg-quick  down wg0
-wg-quick  up   wg0  ||  {
+wg-quick  down wg6
+wg-quick  up   wg6  ||  {
      echo   启动wireguard失败，请检查/etc/wireguard/wg0.conf是否存在错误
 }
 wg
 
 
 
+echo    "--------以下是客户端配置文件，请保存并在客户端中使用---------"
+cat      client4.conf
+cat      client6.conf
 
 
 
