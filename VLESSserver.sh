c0c3f5d0060f7223c30aad814f17feaf523f5cc1
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-12-28 23:35:48 +0800

    Update VLESSserver.sh

--- VLESSserver.sh
+++ VLESSserver.sh
@@ -42,10 +42,11 @@ cp       /etc/letsencrypt/live/$site/*      /home/
 chmod    -Rf     777     /home/
 #配置证书每月1日自动更新
 echo       "
-1 0 1 * *     certbot       renew
-2 0 1 * *     cp           /etc/letsencrypt/live/$site/*          /home/
-3 0 1 * *     chmod        -Rf        777       /home/
-4 0 1 * *     service       v2ray    restart
+1 0 1 * *     service   nginx   stop
+1 0 1 * *     certbot   renew
+2 0 1 * *     cp       /etc/letsencrypt/live/$site/*          /home/
+3 0 1 * *     chmod    -Rf        777       /home/
+4 0 1 * *     service   v2ray    restart
 "      |      crontab
 service       cron      restart
 #修改v2ray配置文件
