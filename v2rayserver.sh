7932d5a667677c7a479c522a8006ffdde3171745
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-01-09 08:00:31 +0800

    Update v2rayserver.sh

--- v2rayserver.sh
+++ v2rayserver.sh
@@ -72,8 +72,8 @@ listen 80;
 listen [::]:80;
 listen 443 ssl;
 listen [::]:443 ssl;
-ssl_certificate          /etc/letsencrypt/live/www.example.com/fullchain.pem;  
-ssl_certificate_key      /etc/letsencrypt/live/www.example.com/privkey.pem;   
+ssl_certificate          /etc/letsencrypt/live/www.example.com/fullchain.pem;
+ssl_certificate_key      /etc/letsencrypt/live/www.example.com/privkey.pem;
 if ( $scheme = http ){return 301 https://$server_name$request_uri;}
 location /         {           #设置反代网站
 sub_filter   $proxy_name   $server_name;
