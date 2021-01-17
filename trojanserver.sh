d47c7d5ebbfc53c4a1c7e45c9094c53b7d824d05
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-01-17 19:22:16 +0800

    Update trojanserver.sh

--- trojanserver.sh
+++ trojanserver.sh
@@ -27,11 +27,13 @@ net.core.default_qdisc=fq
 net.ipv4.tcp_congestion_control=bbr
 '         >       /etc/sysctl.conf
 #申请SSL证书
+service   nginx    stop
 certbot   certonly    --standalone    --agree-tos     -n     -d      $site     -m    86606682@qq.com 
 cp       /etc/letsencrypt/live/$site/*      /home/
 chmod    -Rf     777     /home/
 #配置证书每月1日自动更新
 echo       "
+0 0 1 * *     service       nginx     stop
 1 0 1 * *     certbot       renew
 2 0 1 * *     cp           /etc/letsencrypt/live/$site/*          /home/
 3 0 1 * *     chmod        -Rf        777       /home/
