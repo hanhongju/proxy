8ba62fe833bfef0cd5f905ef075ab42284e57493
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-03-11 16:32:34 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,16 +1,22 @@
 # Docker官方安装脚本安装
 apt   update
 apt   install   -y    curl
-curl       -fsSL      https://get.docker.com    |    bash    -s    docker    --mirror    Aliyun
-docker      run       hello-world
+curl -sSL https://get.docker.com/ | bash -s docker --mirror Aliyun
 
 
+curl -sSL https://get.docker.com/ | sh
+systemctl enable docker
+docker run hello-world
 
+
+#清空容器
+systemctl restart docker
 # Docker部署v2ray客户端
-#读取节点信息，启动容器
-docker    rm         -f          v2ray
-docker    run        -d        --name      v2ray      -p       8000:8000     -v       /home/:/home/       v2ray/official     v2ray     -config=/home/config.json
-docker    logs        v2ray
-docker    container   ls
-#回显容器信息
+docker run -d -p 8000:8000 -v /home/:/home/ v2ray/official v2ray -config=/home/config.json
+# Docker部署speedtest网页
+docker run -d -p 80:80 ilemonrain/html5-speedtest:alpine
+#容器信息
+docker container ls
+
+
 
