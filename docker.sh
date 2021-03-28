29eea1eb8834925d9fae0ed2ffda44355fd5b4a2
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-03-28 08:36:39 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -11,10 +11,16 @@ docker run hello-world
 systemctl restart docker
 # Docker部署v2ray客户端
 docker run -d -p 8000:8000 -v /home/:/home/ v2ray/official v2ray -config=/home/config.json
-# Docker部署speedtest网页
+# Docker部署speedtest
 docker run -d -p 80:80 ilemonrain/html5-speedtest:alpine
+# Docker部署Rstudio server
+docker run -d -p 8787:8787 -e DISABLE_AUTH=true rocker/rstudio
 #容器信息
 docker container ls
+
+
+
+
 
 
 
