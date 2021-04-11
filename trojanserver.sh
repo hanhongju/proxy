aec699818f074a6e3c2c051231f3ee5ffb77df08
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-04-11 22:22:28 +0800

    Update trojanserver.sh

--- trojanserver.sh
+++ trojanserver.sh
@@ -18,7 +18,7 @@ systemctl     stop        nginx apache2
 certbot       certonly    --standalone -n --agree-tos -m 86606682@qq.com -d $site
 chmod         -R   777    /etc/letsencrypt/
 #配置证书每月1日自动更新
-echo       "
+echo    "
 0 0 1 * *     systemctl     stop        nginx apache2
 1 0 1 * *     certbot       renew
 2 0 1 * *     chmod         -R   777    /etc/letsencrypt/
