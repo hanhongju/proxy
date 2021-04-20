a6ec9b75c26b82d47bd2d18f4f98e3d052560eca
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-04-20 08:34:26 +0800

    Update v2rayserver.sh

--- v2rayserver.sh
+++ v2rayserver.sh
@@ -38,18 +38,15 @@ net.ipv4.tcp_congestion_control=bbr
 #准备sni转发配置
 echo '
 stream {
-map $ssl_preread_server_name $backend_name {
-default web;
-vmess.example.com vmess;
-trojan.example.com trojan;
+map $ssl_preread_server_name $destination {
+default             127.0.0.1:10240;
+vmess.example.com   127.0.0.1:10241;
+trojan.example.com  127.0.0.1:10242;
 }
-upstream web {server 127.0.0.1:10240;}
-upstream vmess {server 127.0.0.1:10241;}
-upstream trojan {server 127.0.0.1:10242;}
 server {
-listen 443 reuseport;
+listen 443;
 listen [::]:443 reuseport;
-proxy_pass  $backend_name;
+proxy_pass  $destination;
 ssl_preread on;
 }
 }
