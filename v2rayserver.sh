29eb14b704c772c72ab5dc4a3c7d65e8cf8f2b0c
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-05-16 00:21:52 +0800

    Update v2rayserver.sh

--- v2rayserver.sh
+++ v2rayserver.sh
@@ -21,6 +21,7 @@ apt    update
 apt    install   -y       curl nginx certbot wget unzip
 wget   https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh   -cP    /home/
 wget   https://github.com/v2fly/v2ray-core/releases/download/v4.38.3/v2ray-linux-64.zip      -cP    /home/
+sed    -i       ''s/read.*//g''          /home/install-release.sh
 bash   /home/install-release.sh    -l    /home/v2ray-linux-64.zip
 systemctl     stop        nginx apache2
 certbot       certonly    --standalone -n --agree-tos -m 86606682@qq.com -d $site
