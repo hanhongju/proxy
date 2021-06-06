483d905b375f3113e06f4bfd4efea148f3f528aa
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-06-07 06:26:43 +0800

    Update v2rayA.sh

--- v2rayA.sh
+++ v2rayA.sh
@@ -1,6 +1,6 @@
 # V2rayA客户端安装脚本 @ Debian 10 or Ubuntu 20.04
 apt    update
-apt    install   -y       wget
+apt    install   -y      wget
 wget   https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh   -cP    /home/
 wget   https://github.com/v2fly/v2ray-core/releases/download/v4.38.3/v2ray-linux-64.zip      -cP    /home/
 sed    -i       ''s/read.*//g''          /home/install-release.sh
