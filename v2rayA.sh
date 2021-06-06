43711cc6523a23b72073678065a90ad001e44a56
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-06-07 06:29:24 +0800

    Update v2rayA.sh

--- v2rayA.sh
+++ v2rayA.sh
@@ -7,7 +7,7 @@ sed    -i       ''s/read.*//g''          /home/install-release.sh
 bash   /home/install-release.sh    -l    /home/v2ray-linux-64.zip
 wget   https://github.com/v2rayA/v2rayA/releases/download/v1.3.3/installer_debian_amd64_v1.3.3.deb     -cP    /home/
 apt    install    -y    /home/installer_debian_amd64_v1.3.3.deb
-# 访问http://localhost:2017/进行客户端节点配置
+# 访问http://localhost:2017/进行客户端节点配置，socks5代理端口20170
 
 
 
