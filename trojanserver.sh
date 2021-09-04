1e2186faadcb2694acb1aa4c3560209cca5d4dff
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-09-04 08:05:21 +0800

    Update trojanserver.sh

--- trojanserver.sh
+++ trojanserver.sh
@@ -50,7 +50,6 @@ systemctl     restart     trojan cron
 trojan     -t
 sysctl     -p
 crontab    -l
-ss         -plnt
 if          [[  $(ss   -plnt     2>&1 )   =~   trojan   ]]
 then        echo   "至此，trojan可正常工作。服务器密码为fengkuang。"
 else        echo   "您输入的域名地址可能没有正确解析或者短时间申请了太多的证书，不能正常申请证书，所以trojan不能正常工作。在您确认了域名解析没有问题后再请重新运行本脚本。"
