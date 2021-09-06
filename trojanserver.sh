c07c86aa39c6077ad64d1ff7561b53fccf418947
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-09-06 18:20:04 +0800

    Update trojanserver.sh

--- trojanserver.sh
+++ trojanserver.sh
@@ -47,9 +47,9 @@ sed     -i      ''s/trojan.example.com/$site/g''       /etc/trojan/config.json
 systemctl     enable      trojan cron
 systemctl     restart     trojan cron
 #验证配置文件，显示监听端口
-trojan     -t
-sysctl     -p
-crontab    -l
+trojan      -t
+sysctl      -p
+crontab     -l
 if          [[  $(ss   -plnt     2>&1 )   =~   trojan   ]]
 then        echo   "至此，trojan可正常工作。服务器密码为fengkuang。"
 else        echo   "您输入的域名地址可能没有正确解析或者短时间申请了太多的证书，不能正常申请证书，所以trojan不能正常工作。在您确认了域名解析没有问题后再请重新运行本脚本。"
