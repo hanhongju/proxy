355880bf386cdd3a8ef9fb03b03e1cb35059b9c3
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-10-17 21:32:35 +0800

    Update xrayserver.sh

--- xrayserver.sh
+++ xrayserver.sh
@@ -17,13 +17,13 @@ sleep   5s
 #计时
 begin=$(date +%s)
 #安装软件申请证书
-apt           update
-apt           -y    install     wget nginx certbot
+apt           -y    update    
+apt           -y    install         wget nginx certbot
 wget          -c    https://github.com/XTLS/Xray-install/raw/main/install-release.sh
 bash          install-release.sh    install
-systemctl     stop        nginx apache2
-certbot       certonly    --standalone -n --agree-tos -m 86606682@qq.com -d $site
-chmod         -R   777    /etc/letsencrypt/
+systemctl     stop                  nginx apache2
+certbot       certonly              --standalone -n --agree-tos -m 86606682@qq.com -d $site
+chmod         -R    777             /etc/letsencrypt/
 #配置证书自动更新
 echo    "
 0 0 1 * *     systemctl     stop        nginx apache2
