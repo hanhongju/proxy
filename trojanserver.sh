d23fef5d312c8653cb490315d5ee2c4fa6a4427d
hanhongju <63781648+hanhongju@users.noreply.github.com>
2022-10-23 16:03:40 +0800

    Update trojanserver.sh

--- trojanserver.sh
+++ trojanserver.sh
@@ -21,8 +21,7 @@ echo    '
 1 0 1 * *     certbot       renew
 2 0 1 * *     chmod         -R   777    /etc/letsencrypt/
 3 0 * * *     systemctl     restart     nginx trojan
-'       >     /home/crontmp
-crontab       -u    root    /home/crontmp
+'       |     crontab
 #修改系统控制文件启用BBR
 echo     '
 net.core.default_qdisc=fq
