fd3a0861d68d86a0fe442b29f799fb020193de2b
hanhongju <63781648+hanhongju@users.noreply.github.com>
2022-11-08 19:19:54 +0800

    Update v2rayclient.sh

--- v2rayclient.sh
+++ v2rayclient.sh
@@ -19,6 +19,7 @@ echo '
                ,"settings": {"vnext": [{"address": "www.example.com"
                                        ,"port": 443
                                        ,"users": [{"id": "8c38d360-bb8f-11ea-9ffd-c182155e578a"}]
+                                       ,"encryption":"none"
                                       }]
                             }
               ,"streamSettings": {"network": "ws"
