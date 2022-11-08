dccb0062bbe8adcecab4060b81e69e852d8f9f19
hanhongju <63781648+hanhongju@users.noreply.github.com>
2022-11-08 20:06:52 +0800

    Update trojanclient.sh

--- trojanclient.sh
+++ trojanclient.sh
@@ -33,7 +33,7 @@ tsocks      wget      https://cn.wordpress.org/latest-zh_CN.tar.gz     -O      t
 
 
 uninstall () {
-apt           -y        remove     trojan
+apt    -y     remove    trojan
 systemctl     stop      trojan
 systemctl     disable   trojan
 netstat       -plnt
