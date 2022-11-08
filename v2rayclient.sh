f83d38101f70712ae1b6b9b485bdd3edc6a10454
hanhongju <63781648+hanhongju@users.noreply.github.com>
2022-11-08 19:07:13 +0800

    Create v2rayclient.sh

---
+++ v2rayclient.sh
@@ -0,0 +1,43 @@
+# v2rayserver安装脚本 @ Debian 11
+site=cloud.aboutnote.live
+apt   -y   update
+apt   -y   install     wget curl tsocks v2ray net-tools
+echo '
+server       =  127.0.0.1
+server_type  =  5
+server_port  =  8080
+default_user =  none
+default_pass =  none
+'           >           /etc/tsocks.conf
+echo '
+{"inbounds": [{"port": 8080
+              ,"protocol": "socks"
+              ,"settings": {"auth": "noauth"
+                           }
+             }]
+,"outbounds": [{"protocol": "vless"
+               ,"settings": {"vnext": [{"address": "www.example.com"
+                                       ,"port": 443
+                                       ,"users": [{"id": "8c38d360-bb8f-11ea-9ffd-c182155e578a"}]
+                                      }]
+                            }
+              ,"streamSettings": {"network": "ws"
+                                 ,"security": "tls"
+                                 }
+              }]
+}
+'                     >                                   /etc/v2ray/config.json
+sed         -i        "s/www.example.com/$site/g"         /etc/v2ray/config.json
+systemctl   enable    v2ray
+systemctl   restart   v2ray
+v2ray       -test     -config=/etc/v2ray/config.json
+netstat     -plnt
+curl        -x        socks5://127.0.0.1:8080        google.com
+tsocks      wget      https://cn.wordpress.org/latest-zh_CN.tar.gz     -O      testdownloadfile
+
+
+
+
+
+
+
