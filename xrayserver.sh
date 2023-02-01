bfc57d256cca721ed758a129b82b09f7bb06367f
hanhongju <63781648+hanhongju@users.noreply.github.com>
2023-02-01 14:38:30 +0800

    Update xrayserver.sh

--- xrayserver.sh
+++ xrayserver.sh
@@ -102,3 +102,17 @@ bash    setup.sh
 
 
 
+uninstall () {
+sudo          su
+apt    -y     remove    xray
+systemctl     stop      xray
+systemctl     disable   xray
+rm            /etc/nginx/sites-enabled/xray.conf
+systemctl     restart   nginx
+netstat       -plnt
+
+}
+
+
+
+
