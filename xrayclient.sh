b62b828f0d9673695ed1fb3c9c45e6df38754ea0
hanhongju <63781648+hanhongju@users.noreply.github.com>
2023-02-06 13:58:04 +0800

    Update xrayclient.sh

--- xrayclient.sh
+++ xrayclient.sh
@@ -3,8 +3,8 @@ site=gcphk.aboutnote.live
 #安装软件
 apt     -y     update
 apt     -y     install     wget curl tsocks net-tools
-wget    http://www.hanhongju.com/Xray-install.sh       -cP      .
-wget    http://www.hanhongju.com/Xray-linux-64.zip     -cP      .
+wget    -cP.   http://www.hanhongju.com/Xray-install.sh
+wget    -cP.   http://www.hanhongju.com/Xray-linux-64.zip
 bash    Xray-install.sh    -l    Xray-linux-64.zip
 #写入配置文件
 echo '
