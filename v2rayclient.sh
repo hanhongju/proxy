ca46f60aa3722df3aa26a334e27f7aa688c300cd
hanhongju <63781648+hanhongju@users.noreply.github.com>
2023-02-09 13:00:51 +0800

    Update v2rayclient.sh

--- v2rayclient.sh
+++ v2rayclient.sh
@@ -44,7 +44,7 @@ tsocks      wget      https://cn.wordpress.org/latest-zh_CN.tar.gz     -O      t
 
 
 uninstall () {
-sudo          su
+sudo   su
 apt    -y     remove    v2ray
 systemctl     stop      v2ray
 systemctl     disable   v2ray
