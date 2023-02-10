a88d4cf071118cf9d8738f52b41f5ecad630fba1
hanhongju <63781648+hanhongju@users.noreply.github.com>
2023-02-10 12:47:04 +0800

    Update auto_update_cert.sh

--- auto_update_cert.sh
+++ auto_update_cert.sh
@@ -4,9 +4,6 @@ echo    '
 0 1 * * *     apt           -y          update
 0 2 * * *     apt           -y          full-upgrade
 0 3 * * *     apt           -y          autoremove
-0 4 * * *     mkdir         -p          /home/wordpressbackup/
-0 5 * * *     mysqldump     -uroot      -pfengkuang     wordpress     >    /home/wordpress/wordpress.sql
-0 6 * * *     tar           -cf         /home/wordpressbackup/wordpress$(date +\%Y\%m\%d\-\%H\%M\%S).tar        -P       /home/wordpress/
 0 0 1 * *     systemctl     stop        nginx apache2
 1 0 1 * *     certbot       renew
 2 0 1 * *     chmod         -R   777    /etc/letsencrypt/
