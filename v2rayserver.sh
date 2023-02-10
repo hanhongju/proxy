0dc01c7b2863f55cea724eabdc277a341d3ce499
hanhongju <63781648+hanhongju@users.noreply.github.com>
2023-02-10 12:20:58 +0800

    Update v2rayserver.sh

--- v2rayserver.sh
+++ v2rayserver.sh
@@ -16,17 +16,6 @@ apt           -y   install  certbot v2ray net-tools
 systemctl     stop          nginx apache2
 certbot       certonly      --standalone -n --agree-tos -m 86606682@qq.com -d $site
 chmod         -R   777      /etc/letsencrypt/
-#配置证书自动更新
-echo    '
-* * * * *     date          >>          /home/crontest
-0 1 * * *     apt           -y          update
-0 2 * * *     apt           -y          full-upgrade
-0 3 * * *     apt           -y          autoremove
-0 0 1 * *     systemctl     stop        nginx apache2
-1 0 1 * *     certbot       renew
-2 0 1 * *     chmod         -R   777    /etc/letsencrypt/
-3 0 * * *     systemctl     restart     nginx v2ray
-'       |     crontab
 #修改系统控制文件启用BBR
 echo     '
 net.core.default_qdisc=fq
@@ -54,7 +43,6 @@ sed         -i           "s/www.example.com/$site/g"         /etc/v2ray/config.j
 systemctl   enable       v2ray nginx cron
 systemctl   restart      v2ray nginx cron
 v2ray       -test        -config=/etc/v2ray/config.json
-crontab     -l
 sysctl      -p
 netstat     -plnt
 
@@ -65,6 +53,17 @@ directsetup () {
 sudo    su
 apt     -y    install    wget
 wget    https://github.com/hanhongju/proxy/raw/master/v2rayserver.sh    -O    setup.sh
+bash    setup.sh
+
+}
+
+
+
+
+autoupdatecert () {
+sudo    su
+apt     -y    install    wget
+wget    https://github.com/hanhongju/proxy/raw/master/autoupdatecert.sh    -O    setup.sh
 bash    setup.sh
 
 }
