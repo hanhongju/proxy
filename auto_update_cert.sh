b17671b04e5916e7d1da32c66af70b01fd7f1e2f
hanhongju <63781648+hanhongju@users.noreply.github.com>
2023-05-07 10:32:39 +0800

    Update auto_update_cert.sh

--- auto_update_cert.sh
+++ auto_update_cert.sh
@@ -5,7 +5,7 @@ echo    '
 0 1 * * *     apt           -y          update
 0 2 * * *     apt           -y          full-upgrade
 0 3 * * *     apt           -y          autoremove
-0 0 1 * *     systemctl     stop        nginx apache2
+0 0 1 * *     systemctl     stop        nginx
 1 0 1 * *     certbot       renew
 2 0 1 * *     chmod         -R   777    /etc/letsencrypt/
 3 0 * * *     systemctl     restart     nginx trojan
