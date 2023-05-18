d85d09f74a66af12675c108351d594f3f641fc59
hanhongju <63781648+hanhongju@users.noreply.github.com>
2023-05-19 04:36:09 +0800

    Update certificate_request.sh

--- certificate_request.sh
+++ certificate_request.sh
@@ -3,7 +3,7 @@ sudo  su
 apt   -y    update
 apt   -y    install    certbot python3-pip
 pip         install    certbot-dns-cloudflare
-echo        "dns_cloudflare_api_token = dxf923blezqeB2jVXP_dGGsPBuGg0nRh4FUvpMD4"       >       /home/cloudflare_credentials.ini
+echo        "dns_cloudflare_api_token = jPOSoygxMtPyzr7I47YO3WWA4WrnmFFRgc0xYZ3l"       >       /home/cloudflare_credentials.ini
 certbot     certonly   --dns-cloudflare   -m   86606682@qq.com   --dns-cloudflare-credentials   /home/cloudflare_credentials.ini    -d    *.hanhongju.com    --config-dir     /home/
 
 
