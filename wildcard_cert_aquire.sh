0ca902e2ab0d862642cf232bd5e88a6873122819
hanhongju <63781648+hanhongju@users.noreply.github.com>
2023-05-19 04:19:05 +0800

    Update wildcard_cert_aquire.sh

--- wildcard_cert_aquire.sh
+++ wildcard_cert_aquire.sh
@@ -1,9 +1,9 @@
 
 apt   -y    update
-apt   -y    install       certbot python3-pip
-pip         install       certbot-dns-cloudflare
-echo        "dns_cloudflare_api_token = dxf923blezqeB2jVXP_dGGsPBuGg0nRh4FUvpMD4"     >      /home/cloudflare_credentials.ini
-certbot     certonly      --dns-cloudflare        --dns-cloudflare-credentials        /home/cloudflare_credentials.ini      -d      *.hanhongju.com
+apt   -y    install    certbot python3-pip
+pip         install    certbot-dns-cloudflare
+echo        "dns_cloudflare_api_token = dxf923blezqeB2jVXP_dGGsPBuGg0nRh4FUvpMD4"       >       /home/cloudflare_credentials.ini
+certbot     certonly   --dns-cloudflare   -m   86606682@qq.com   --dns-cloudflare-credentials   /home/cloudflare_credentials.ini    -d    *.hanhongju.com    --config-dir     /home/
 
 
 
