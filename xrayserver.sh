37de82fbf92ca764d0920707b9cab74b16459197
hanhongju <63781648+hanhongju@users.noreply.github.com>
2023-05-19 20:18:58 +0800

    Update xrayserver.sh

--- xrayserver.sh
+++ xrayserver.sh
@@ -8,9 +8,9 @@ pip         install    certbot-dns-cloudflare
 wget  -c    https://github.com/XTLS/Xray-install/raw/main/install-release.sh
 bash        install-release.sh     install
 echo        "dns_cloudflare_api_token = jPOSoygxMtPyzr7I47YO3WWA4WrnmFFRgc0xYZ3l"       >       /home/cloudflare_credentials.ini
-certbot     certonly  --agree-tos  --eff-email  -m  86606682@qq.com  --dns-cloudflare  
-            \--dns-cloudflare-credentials  /home/cloudflare_credentials.ini  -d  *.$site  
-            \--deploy-hook  "chmod -R 777 /etc/letsencrypt/" 
+certbot     certonly  --agree-tos  --eff-email  -m  86606682@qq.com  --dns-cloudflare\
+            --dns-cloudflare-credentials  /home/cloudflare_credentials.ini  -d  *.$site\
+            --deploy-hook  "chmod -R 777 /etc/letsencrypt/" 
 cp          /etc/letsencrypt/live/$site/fullchain.pem     /home/fullchain.pem
 cp          /etc/letsencrypt/live/$site/privkey.pem       /home/privkey.pem
 echo     '
