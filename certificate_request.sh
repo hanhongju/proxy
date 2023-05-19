bdc2afa6528354c353070bcce2241b7e9458f99a
hanhongju <63781648+hanhongju@users.noreply.github.com>
2023-05-19 14:13:53 +0800

    Update certificate_request.sh

--- certificate_request.sh
+++ certificate_request.sh
@@ -5,10 +5,9 @@ apt   -y    update
 apt   -y    install    certbot python3-pip
 pip         install    certbot-dns-cloudflare
 echo        "dns_cloudflare_api_token = jPOSoygxMtPyzr7I47YO3WWA4WrnmFFRgc0xYZ3l"       >       /home/cloudflare_credentials.ini
-certbot     certonly  --agree-tos  --eff-email  -m  86606682@qq.com  --dns-cloudflare  --dns-cloudflare-credentials  /home/cloudflare_credentials.ini  -d  *.$site
-chmod   -R  777  /etc/letsencrypt/
-cp      /etc/letsencrypt/live/$site/fullchain.pem     /home/fullchain.pem
-cp      /etc/letsencrypt/live/$site/privkey.pem       /home/privkey.pem
+certbot     certonly  --agree-tos  --eff-email  -m  86606682@qq.com  --dns-cloudflare  --dns-cloudflare-credentials  /home/cloudflare_credentials.ini  -d  *.$site  --deploy-hook  "chmod -R 777 /etc/letsencrypt/" 
+cp          /etc/letsencrypt/live/$site/fullchain.pem     /home/fullchain.pem
+cp          /etc/letsencrypt/live/$site/privkey.pem       /home/privkey.pem
 
 
 
