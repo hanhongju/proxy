8b65d5e5c5f481e780109ea8f91ba87673a54944
hanhongju <63781648+hanhongju@users.noreply.github.com>
2023-05-20 08:13:58 +0800

    Update trojanserver.sh

--- trojanserver.sh
+++ trojanserver.sh
@@ -8,7 +8,9 @@ sleep   5s
 #安装软件申请证书
 apt    -y     update
 apt    -y     install       certbot trojan nginx net-tools
-certbot       certonly      --standalone  -n  --agree-tos  -m  86606682@qq.com  -d  $site  --pre-hook  "systemctl stop nginx"  --post-hook  "systemctl start nginx"  --deploy-hook "chmod -R 777 /etc/letsencrypt/" 
+certbot       certonly      --standalone  -n  --agree-tos  -m  86606682@qq.com  -d  $site\
+              --pre-hook  "systemctl stop nginx"  --post-hook  "systemctl start nginx"\
+              --deploy-hook "chmod -R 777 /etc/letsencrypt/" 
 #修改系统控制文件启用BBR
 echo     '
 net.core.default_qdisc=fq
