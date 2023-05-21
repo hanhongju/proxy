88ffb9cffecf3e4344b7605936c2241f13011c80
hanhongju <63781648+hanhongju@users.noreply.github.com>
2023-05-22 01:49:29 +0800

    Update certificate_autoupdate.sh

--- certificate_autoupdate.sh
+++ certificate_autoupdate.sh
@@ -1,11 +1,11 @@
 #定期更新证书
 sudo    su
 echo    '
-* * * * *     date          >>          /home/crontest
-0 1 * * *     apt           -y          update
-0 2 * * *     apt           -y          full-upgrade
-0 3 * * *     apt           -y          autoremove
-1 0 1 * *     certbot       renew       --pre-hook "systemctl stop nginx"      --post-hook "systemctl restart nginx trojan"
+* * * * *     date   >>    /home/crontest
+0 1 * * *     apt    -y    update
+0 2 * * *     apt    -y    full-upgrade
+0 3 * * *     apt    -y    autoremove
+1 0 1 * *     certbot      renew
 '       |     crontab
 crontab       -l
 netstat       -plnt
