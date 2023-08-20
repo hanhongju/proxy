05c83b90bb486f77d845ff356bdf6382886b33a4
hanhongju <63781648+hanhongju@users.noreply.github.com>
2023-08-21 00:43:41 +0800

    Update trojanserver.sh

--- trojanserver.sh
+++ trojanserver.sh
@@ -18,7 +18,7 @@ echo    '
 0 1 * * *      apt    -y    update
 0 2 * * *      apt    -y    full-upgrade
 0 3 * * *      apt    -y    autoremove
-1 0 1 * *      certbot      renew
+0 4 * * *      certbot      renew
 '       |      crontab
 echo    '
 server{
