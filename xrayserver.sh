c360dfb22798a27b81c3808437caee22e3731b57
hanhongju <63781648+hanhongju@users.noreply.github.com>
2023-08-20 19:54:35 +0800

    Update xrayserver.sh

--- xrayserver.sh
+++ xrayserver.sh
@@ -75,7 +75,7 @@ netstat     -plnt
 
 directsetup () {
 apt     -y    install    wget
-wget    https://github.com/hanhongju/proxy/raw/master/xrayserver.sh    -O    setup.sh
+wget    https://raw.githubusercontent.com/hanhongju/proxy/master/xrayserver.sh    -O    setup.sh
 bash    setup.sh
 
 }
