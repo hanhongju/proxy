638b29a3847d02586909b6c52615200b2a944ff7
hanhongju <63781648+hanhongju@users.noreply.github.com>
2024-12-22 17:24:43 +0800

    Update trojanserver.sh

--- trojanserver.sh
+++ trojanserver.sh
@@ -24,7 +24,7 @@ echo    '
 {"run_type": "server"
 ,"local_addr": "::"
 ,"local_port": 443
-,"remote_addr": "www.naenara.com.kp"
+,"remote_addr": "www.rodong.rep.kp"
 ,"remote_port": 80
 ,"password": ["fengkuang"]
 ,"ssl": {"cert": "/etc/letsencrypt/live/www.example.com/fullchain.pem"
