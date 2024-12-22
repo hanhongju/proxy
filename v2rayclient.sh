8206fac682f5802f0203fec9c109b558e9164f04
hanhongju <63781648+hanhongju@users.noreply.github.com>
2024-12-22 16:56:26 +0800

    Create v2rayclient.sh

---
+++ v2rayclient.sh
@@ -0,0 +1,52 @@
+site=gcphk.aboutnote.live
+apt     -y     update
+apt     -y     install     wget curl tsocks net-tools v2ray
+echo '
+server       =  127.0.0.1
+server_type  =  5
+server_port  =  8080
+default_user =  none
+default_pass =  none
+'            >           /etc/tsocks.conf
+echo '
+{"inbounds": [{"port": 8080
+              ,"protocol": "socks"
+              ,"settings": {"auth": "noauth"
+                           }
+              ,"sniffing": {"enabled": true
+                           ,"destOverride": ["http", "tls"]
+                           }
+             }]
+,"outbounds": [{"protocol": "vmess"
+               ,"settings": {"vnext": [{"address": "www.example.com"
+                                       ,"port": 443
+                                       ,"users": [{"id": "8c38d360-bb8f-11ea-9ffd-c182155e578a"
+                                                  ,"encryption": "none"
+                                                 }]
+                                      }]
+                            }
+              ,"streamSettings": {"network": "ws"
+                                 ,"security": "tls"
+                                 ,"wsSettings": {"path": "/world"}
+                                 }
+              }]
+}
+'           >                                             /usr/local/etc/xray/config.json
+sed         -i        "s/www.example.com/$site/g"         /usr/local/etc/xray/config.json
+systemctl   enable    xray
+systemctl   restart   xray
+xray        -test     -config=/usr/local/etc/xray/config.json
+netstat     -plnt
+curl        -x        socks5://127.0.0.1:8080        google.com
+tsocks      wget      https://cn.wordpress.org/latest-zh_CN.tar.gz     -O      testdownloadfile
+
+
+
+
+uninstall () {
+systemctl     stop      xray
+systemctl     disable   xray
+netstat       -plnt
+
+}
+
