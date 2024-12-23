67ef7c1af32e1422771f1e6cea90c01b2b4f92b6
hanhongju <63781648+hanhongju@users.noreply.github.com>
2024-12-23 10:33:56 +0800

    Update ssserver.sh

--- ssserver.sh
+++ ssserver.sh
@@ -1,22 +1,21 @@
 apt     -y     update
-apt     -y     install     v2ray net-tools
+apt     -y     install      net-tools shadowsocks-libev
 echo     '
 net.core.default_qdisc=fq
 net.ipv4.tcp_congestion_control=bbr
 '         >       /etc/sysctl.conf
 echo '
-{"inbounds": [{"port": 10086
-              ,"protocol": "shadowsocks"
-              ,"settings": {"method": "aes-256-gcm"
-                           ,"password": "fengkuang"
-                           }
-             }]
-,"outbounds": [{"protocol": "freedom"}]
+{
+    "server":["::0","0.0.0.0"],
+    "server_port":10086,
+    "fast_open":true,
+    "method":"aes-256-gcm",
+    "password":"fengkuang",
+    "mode":"tcp_and_udp"
 }
-'           >            /etc/v2ray/config.json
-systemctl   enable       v2ray
-systemctl   restart      v2ray
-v2ray       -test        -config=/etc/v2ray/config.json
+'           >            /etc/shadowsocks-libev/config.json
+systemctl   enable       shadowsocks-libev
+systemctl   restart      shadowsocks-libev
 netstat     -plnt
 
 
@@ -33,4 +32,4 @@ bash    setup.sh
 
 
 
-# shadowsocks安装脚本 @ Debian 11
+# shadowsocks安装脚本 @ Debian 10 or Ubuntu 20
