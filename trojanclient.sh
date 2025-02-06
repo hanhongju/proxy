f330538d591a11d67b714a1e0baee5b3d88cf3df
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-02-06 10:04:38 +0800

    Update trojanclient.sh

--- trojanclient.sh
+++ trojanclient.sh
@@ -25,7 +25,6 @@ systemctl   enable    trojan
 systemctl   restart   trojan
 trojan      -t
 netstat     -plnt
-curl        -x        socks5://127.0.0.1:8080        google.com
 tsocks      wget      https://cn.wordpress.org/latest-zh_CN.tar.gz     -O      testdownloadfile
 
 
