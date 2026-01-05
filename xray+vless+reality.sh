9e67103c4de289c85553ee329787e4aea204ad46
hanhongju <63781648+hanhongju@users.noreply.github.com>
2026-01-06 02:03:34 +0800

    Update and rename vless+reality.sh to xray+vless+reality.sh

--- xray+vless+reality.sh
+++ xray+vless+reality.sh
@@ -41,6 +41,6 @@ netstat     -plnt
 
 
 
-# vless+reality安装脚本 @ Debian 12
+# xray+vless+reality安装脚本 @ Debian 12
 # Password: 1psC7ZNJWFAj8zG9mGM1EGibv17mQKoujkLEixNWJyo
 # 本脚本无需申请tls证书。借用其他网站的tls证书保护流量。
