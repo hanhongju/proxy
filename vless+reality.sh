8c3af049fe93c8b267efd37893b2e9db7e8baec8
hanhongju <63781648+hanhongju@users.noreply.github.com>
2026-01-06 00:26:48 +0800

    Update vless+reality.sh

--- vless+reality.sh
+++ vless+reality.sh
@@ -6,7 +6,7 @@ apt     -y    install     net-tools curl
 bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install
 
 echo        '
-{"inbounds": [{"port": 8964
+{"inbounds": [{"port": 443
               ,"protocol": "vless"
               ,"settings": {"clients": [{"id": "8c38d360-bb8f-11ea-9ffd-c182155e578a"}]
                            ,"decryption": "none"
@@ -24,9 +24,18 @@ echo        '
 ,"outbounds":[{"protocol": "freedom"}]
 }
 '            >            /usr/local/etc/xray/config.json
+xray        -test       -config=/usr/local/etc/xray/config.json
 
 
 
-xray       -test       -config=/usr/local/etc/xray/config.json
+
 systemctl   enable      xray
 systemctl   restart     xray
+netstat     -plnt
+
+
+
+
+
+# v2rayserver安装脚本 @ Debian 11 or Ubuntu 22
+# v2ray的VMESS协议可配合Netch代理UDP协议的网络游戏数据包，VLESS协议不可以。该配置可在v2rayN v5客户端运行。可使用Cloudflare 中转流量。
