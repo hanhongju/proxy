c977711505d16c3a3c0af24d816cd26e8c2d09df
hanhongju <63781648+hanhongju@users.noreply.github.com>
2026-01-05 23:50:07 +0800

    Update v2ray+vmess+ws+tls_server.sh

--- v2ray+vmess+ws+tls_server.sh
+++ v2ray+vmess+ws+tls_server.sh
@@ -85,4 +85,4 @@ bash    setup.sh
 
 
 # v2rayserver安装脚本 @ Debian 11 or Ubuntu 22
-# v2ray的VMESS协议可配合Netch代理UDP协议的网络游戏数据包，VLESS协议不可以。该配置可在v2rayN v5客户端运行。
+# v2ray的VMESS协议可配合Netch代理UDP协议的网络游戏数据包，VLESS协议不可以。该配置可在v2rayN v5客户端运行。可使用Cloudflare 中转流量。
